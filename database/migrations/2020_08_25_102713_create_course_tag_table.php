26629c7f355a5b2b1d0054f963dc758f5d548438
linhchi2406 <linhchi24060508@gmail.com>
2020-08-26 11:41:48 +0700

    Add Database + Model

--- database/migrations/2020_08_25_102713_create_course_tag_table.php
+++ database/migrations/2020_08_25_102713_create_course_tag_table.php
@@ -15,7 +15,7 @@ class CreateCourseTagTable extends Migration
     public function up()
     {
         Schema::create('course_tag', function (Blueprint $table) {
-            $table->integer('course_id');
+            $table->integer('course_id')->nullable();
             $table->integer('tag_id');
             $table->timestamps();
             $table->softDeletes();
